c1a51856ca2b792b0cd22352f04889bb5883e2a6
tommib97 <tommaso.bosetti@gmail.com>
2018-10-04 09:47:49 +0200

    create client

---
+++ client.js
@@ -0,0 +1,18 @@
+const fetch = require("node-fetch");
+// const url = "https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400"
+
+const url = "http://localhost:3000/courses"
+ 
+const getLocation = async url => {
+ 
+  try {
+    const response = await fetch(url);
+    const json = await response.json();
+    console.log(json)
+  } catch (error) {
+    console.log(error);
+  }
+};
+ 
+ 
+getLocation(url);
